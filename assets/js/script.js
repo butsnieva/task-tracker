8e94477acd4c3b9772dfdb2df37a06c0903e7bbc
Kateryna B <butsneva.ekaterina@gmail.com>
2021-04-17 22:15:55 -0400

    move task to the correspond column when the status is changing

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var taskIdCounter = 0;
 var pageContentEl = document.querySelector('#page-content')
 var formEl = document.querySelector('#task-form');
 var tasksTodoEl = document.querySelector('#tasks-todo');
+var tasksInProgressEl = document.querySelector('#tasks-in-progress');
+var tasksCompletedEl = document.querySelector('#tasks-completed');
 
 
 var taskFormHandler = function(event) {
@@ -142,6 +144,24 @@ var completeEditTask = function(taskName, taskType, taskId) {
 
     formEl.removeAttribute('data-task-id');
     document.querySelector('#save-task').textContent = 'Add Task';
-}
+};
+
+var taskStatusChangeHandler = function(event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute('data-task-id');
+    //get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
+    //find the parent task item element based on the id
+    var taskSelected = document.querySelector('.task-item[data-task-id="' + taskId + '"]');
+
+        if (statusValue === 'to do') {
+            tasksTodoEl.appendChild(taskSelected);
+        } else if (statusValue === 'in progress') {
+            tasksInProgressEl.appendChild(taskSelected);
+        } else if (statusValue === 'completed') {
+            tasksCompletedEl.appendChild(taskSelected);
+        }
+};
 
 pageContentEl.addEventListener('click', taskButtonHandler);
+pageContentEl.addEventListener('change', taskStatusChangeHandler);
